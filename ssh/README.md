7c3aa0969ee06a053a7dea9be857ad603382b408
Felipe Dornelas <m@felipedornelas.com>
2024-06-28 00:15:33 -0300

    [ssh] revamp ssh configuration and scripts

--- ssh/README.md
+++ ssh/README.md
@@ -1,4 +1,14 @@
 Settings for SSH
 ================
 
-The `install.sh` script will install my settings for SSH. It needs to be run inside this directory.
+First, generate a new SSH key or restore it from backup.
+
+Then, copy `config.template` to `config.local`, enabling the desired entries.
+
+The `install.sh` script will load these settings into the SSH daemon. It needs to be run inside this directory. Before running it , please export the following environment variables:
+
+- `KEY_NAME`: the name of the desired SSH key name located at `~/.ssh`.
+
+Example:
+
+    KEY_NAME=id_github ./install.sh
--- ssh/install.sh
+++ ssh/install.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-SSH_HOME=~/.ssh
-PRIVATE_KEY="${SSH_HOME}/id_rsa"
-CONFIG_TARGET="${SSH_HOME}/config"
-CONFIG_TEMPLATE=config.template
 
-check_directory() {
-    if [ ! -f "${CONFIG_TEMPLATE}" ]
+SSH_HOME=~/.ssh
+CONFIG_TEMPLATE=config.template
+LOCAL_CONFIG=./config.local
+TARGET_CONFIG="${SSH_HOME}/config"
+PRIVATE_KEY_PATH="${SSH_HOME}/${KEY_NAME}"
+
+
+check_environment_variables() {
+    if [[ -z "${KEY_NAME}" ]]
     then
-        echo "${CONFIG_TEMPLATE} not found! Are you running this script inside its directory?"
+        echo "Please set the KEY_NAME environment variable with the desired SSH key name located at ${SSH_HOME}. Example: KEY_NAME=id_github"
+        exit -1
+    fi
+}
+
+check_local_config() {
+    if [ ! -f "${LOCAL_CONFIG}" ]
+    then
+        echo "${LOCAL_CONFIG} not found! Did you copy it from ${CONFIG_TEMPLATE}?"
         exit -1
     fi
 }
 
 check_if_private_key_exists() {
-    if [ ! -f "${PRIVATE_KEY}" ]
+    if [ ! -f "${PRIVATE_KEY_PATH}" ]
     then
-        echo "Could not find private key at ${PRIVATE_KEY}. Please add it and run this script again."
+        echo "Could not find private key at ${PRIVATE_KEY_PATH}. Please add it and run this script again."
         exit -1
     fi
 }
 
 copy_config_template_to_target() {
-    if [ -f "${CONFIG_TARGET}" ]
+    if [ -f "${TARGET_CONFIG}" ]
     then
-
-        read -p "File ${CONFIG_TARGET} already exists. Overwrite? [y/n] " -n 1 -r
+        read -p "File ${TARGET_CONFIG} already exists. Overwrite? [y/n] " -n 1 -r
         echo
 
         if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -35,14 +45,15 @@ copy_config_template_to_target() {
         fi
     fi
 
-    cp "${CONFIG_TEMPLATE}" "${CONFIG_TARGET}"
+    ln -sf "${LOCAL_CONFIG}" "${TARGET_CONFIG}"
 }
 
 add_ssh_identity() {
-    ssh-add ${PRIVATE_KEY}
+    ssh-add ${PRIVATE_KEY_PATH}
 }
 
-check_directory
+check_environment_variables
+check_local_config
 check_if_private_key_exists
 copy_config_template_to_target
 add_ssh_identity
